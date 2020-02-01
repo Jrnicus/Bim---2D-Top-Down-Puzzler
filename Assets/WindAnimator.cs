3891f8e670ca4185076ed567649622e55ce72032
MTrecozzi <47111522+MTrecozzi@users.noreply.github.com>
2020-02-01 12:15:41 -0500

    Added Wind Gust Visualization

---
+++ Assets/WindAnimator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class WindAnimator : MonoBehaviour
+{
+
+    [SerializeField]
+    private Animator windGust;
+
+    public void TriggerGust(){
+        windGust.SetTrigger("Wind");
+    }
+
+    
+}
