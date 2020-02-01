16016e1e6d1ab24d16b39bd6955a15cb0811d6e6
MTrecozzi <47111522+MTrecozzi@users.noreply.github.com>
2020-02-01 11:55:14 -0500

    Added Basic Player

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb2d;
+
+    public SpriteRenderer playerSprite;
+    // Start is called before the first frame update
+
+    Vector2 movement;
+    Vector2 mousePosition;
+
+    void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb2d.MovePosition(rb2d.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+
+        Vector2 lookDir = mousePosition - rb2d.position;
+
+
+
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
+
+        bool flipY = lookDir.x < 0 ? true : false;
+        {
+
+            playerSprite.flipX = !flipY;
+        }
+    }
+}
